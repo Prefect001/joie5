35f07a8c9f7936c4bdc2021701b2db8ae01c9731
Xolile Xaba <xolile@medyear.com>
2022-07-24 01:51:49 +0200

    server changes

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-let express = require('express');
-
-let app = express();
-
+const express = require('express');
+const path = require('path');
+const app = express();
 app.use(express.static(__dirname + '/dist/joie5'));
-
-app.get('/*', (req, resp)=>{
-    resp.sendFile(__dirname+'/dist/joie5/index.html');
-
-});
-
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/joie5/index.html'));});
 app.listen(process.env.PORT || 8080);
